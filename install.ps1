c4625e97b23419bf70404215595c70c4398ce98d
Eric Kang <erickangatms@outlook.com>
2018-02-13 10:17:21 -0800

    .

--- install.ps1
+++
@@ -1,3 +0,0 @@
-
-cpi * ~/.carbon/extensions/ -force -recurse
-cpi * ~/.carbon-dev/extensions/ -force -recurse
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.carbon
-mkdir -p ~/.carbon/extensions
-
-cp -r * ~/.carbon/extensions/
