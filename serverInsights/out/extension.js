75575db051a945b8bf51b6eb3d865f010f9aabb3
Eric Kang <erickangatms@outlook.com>
2018-02-13 16:19:12 -0800

    .

--- serverInsights/out/extension.js
+++ serverInsights/out/extension.js
@@ -8,11 +8,9 @@ const vscode = require("vscode");
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sd" is now active!');
     let config = vscode.workspace.getConfiguration('dashboard.server');
     let current = config.inspect('widgets');
-    console.log(current.defaultValue);
-    let mod = current.defaultValue.concat([
+    let mod = [
         {
             "name": "Wait Counts",
             "widget": {
@@ -22,13 +20,111 @@ function activate(context) {
                 "sizex": 2,
                 "sizey": 1
             }
+        },
+        {
+            "name": "Database Space Usage",
+            "widget": {
+                "extension-dbspace-usage": null
+            },
+            "gridItemConfig": {
+                "sizex": 2,
+                "sizey": 1
+            }
+        },
+        {
+            "name": "Backup Size Growth",
+            "widget": {
+                "extension-backup-growth-trend": null
+            },
+            "gridItemConfig": {
+                "sizex": 2,
+                "sizey": 1
+            }
+        },
+        {
+            "name": "Database Memory Usage",
+            "widget": {
+                "extension-dbbuffer-usage": null
+            },
+            "gridItemConfig": {
+                "sizex": 2,
+                "sizey": 1
+            }
+        },
+        {
+            "name": "CPU Utilization",
+            "widget": {
+                "extension-cpu-utilization": null
+            },
+            "gridItemConfig": {
+                "sizex": 2,
+                "sizey": 1
+            }
+        },
+        {
+            "widget": {
+                "backup-history-server-insight": {
+                    "cacheId": "0c7cba8b-c87a-4bcc-ae54-2f40a5503a90"
+                }
+            }
+        },
+        {
+            "name": "Tasks",
+            "widget": {
+                "tasks-widget": {}
+            },
+            "gridItemConfig": {
+                "sizex": 1,
+                "sizey": 1
+            }
+        },
+        {
+            "name": "Search",
+            "gridItemConfig": {
+                "sizex": 1,
+                "sizey": 1
+            },
+            "widget": {
+                "explorer-widget": {}
+            }
         }
-    ]);
+    ];
+    //mod = mod.concat(current.defaultValue);
     config.update('widgets', mod, vscode.ConfigurationTarget.Global);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
 function deactivate() {
+    let config = vscode.workspace.getConfiguration('dashboard.server');
+    let original = [
+        {
+            "widget": {
+                "backup-history-server-insight": {
+                    "cacheId": "0c7cba8b-c87a-4bcc-ae54-2f40a5503a90"
+                }
+            }
+        },
+        {
+            "name": "Tasks",
+            "widget": {
+                "tasks-widget": {}
+            },
+            "gridItemConfig": {
+                "sizex": 1,
+                "sizey": 1
+            }
+        },
+        {
+            "gridItemConfig": {
+                "sizex": 1,
+                "sizey": 1
+            },
+            "widget": {
+                "explorer-widget": {}
+            }
+        }
+    ];
+    config.update('widgets', original, vscode.ConfigurationTarget.Global);
 }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- serverInsights/src/extension.ts
+++ serverInsights/src/extension.ts
@@ -9,30 +9,123 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "sd" is now active!');
-
     let config = vscode.workspace.getConfiguration('dashboard.server');
-
     let current = config.inspect<Array<any>>('widgets');
-
-    console.log(current.defaultValue);
-
-    let mod = current.defaultValue.concat([
+    let mod = [
         {
             "name": "Wait Counts",
             "widget": {
-              "extension-wait-counts-by-Paul-Randal": null
+                "extension-wait-counts-by-Paul-Randal": null
             },
             "gridItemConfig": {
-              "sizex": 2,
-              "sizey": 1
+                "sizex": 2,
+                "sizey": 1
             }
-          }
-    ]);
+        },
+        {
+            "name": "Database Space Usage",
+            "widget": {
+                "extension-dbspace-usage": null
+            },
+            "gridItemConfig": {
+                "sizex": 2,
+                "sizey": 1
+            }
+        },
+        {
+            "name": "Backup Size Growth",
+            "widget": {
+                "extension-backup-growth-trend": null
+            },
+            "gridItemConfig": {
+                "sizex": 2,
+                "sizey": 1
 
+            }
+        },
+        {
+            "name": "Database Memory Usage",
+            "widget": {
+                "extension-dbbuffer-usage": null
+            },
+            "gridItemConfig": {
+                "sizex": 2,
+                "sizey": 1
+            }
+        },
+        {
+            "name": "CPU Utilization",
+            "widget": {
+                "extension-cpu-utilization": null
+            },
+            "gridItemConfig": {
+                "sizex": 2,
+                "sizey": 1
+            }
+        },
+        {
+            "widget": {
+                "backup-history-server-insight": {
+                    "cacheId": "0c7cba8b-c87a-4bcc-ae54-2f40a5503a90"
+                }
+            }
+        },
+        {
+            "name": "Tasks",
+            "widget": {
+                "tasks-widget": {}
+            },
+            "gridItemConfig": {
+                "sizex": 1,
+                "sizey": 1
+            }
+        },
+        {
+            "name": "Search",
+            "gridItemConfig": {
+                "sizex": 1,
+                "sizey": 1
+            },
+            "widget": {
+                "explorer-widget": {}
+            }
+        }
+    ];
+
+    //mod = mod.concat(current.defaultValue);
     config.update('widgets', mod, vscode.ConfigurationTarget.Global);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+    let config = vscode.workspace.getConfiguration('dashboard.server');
+    let original = [
+        {
+            "widget": {
+                "backup-history-server-insight": {
+                    "cacheId": "0c7cba8b-c87a-4bcc-ae54-2f40a5503a90"
+                }
+            }
+        },
+        {
+            "name": "Tasks",
+            "widget": {
+                "tasks-widget": {}
+            },
+            "gridItemConfig": {
+                "sizex": 1,
+                "sizey": 1
+            }
+        },
+        {
+            "gridItemConfig": {
+                "sizex": 1,
+                "sizey": 1
+            },
+            "widget": {
+                "explorer-widget": {}
+            }
+        }
+    ];
+    config.update('widgets', original, vscode.ConfigurationTarget.Global)
 }
